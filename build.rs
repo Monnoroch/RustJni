9695b745af6d62ce01f15123d5015fbcbb19bc25
Demetrios Obenour <demetriobenour@gmail.com>
2015-05-16 22:18:53 -0400

    Cleaned up .gitignore, simplified build.rs, and deleted run.sh.

--- build.rs
+++ build.rs
@@ -1,9 +1,8 @@
 use std::env;
 fn main () -> () {
-    match env::var("LD_LIBRARY_PATH") {
-        Ok(var) => for i in env::split_paths(&var) {
-            println!("cargo:rustc-link-search={}",i.to_str().unwrap())
-        },
-        Err(_) => ()
+    if let Ok(var) = env::var("LD_RUN_PATH") {
+        for i in env::split_paths(&var) {
+            println!("cargo:rustc-link-search={}",i.to_str().unwrap());
+        }
     }
 }
