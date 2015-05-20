fdd115f9e74bc610959527ba7cb15a2c0e423f92
Demetrios Obenour <demetriobenour@gmail.com>
2015-05-20 11:11:10 -0400

    Fixed compilation and linking, and reformatted tests/main.rs

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 use std::env;
 fn main () -> () {
-    if let Ok(var) = env::var("LD_RUN_PATH") {
+    if let Ok(var) = env::var("LD_LIBRARY_PATH") {
         for i in env::split_paths(&var) {
             println!("cargo:rustc-link-search={}",i.to_str().unwrap());
         }
